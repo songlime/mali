5089abbabbebb099aad5234c09f23f2a6a2c3f4b
songlime <songlime@yahoo.com>
2015-12-16 22:42:35 +0800

    modify

---
+++ cre/cls.php
@@ -0,0 +1,31 @@
+<?php
+/*
+基础逻辑类
+*/
+class bse_cls{
+	private cls_nme='bse_cls';
+	private nme;
+
+	/*
+	构造函数
+	*/
+	void public function __construct(){
+		parent::__construct();
+	}
+
+	/*
+	析构函数
+	*/
+	void public function __destruct($value=''){
+		
+	}
+
+	/*
+	返回函数名
+	*/
+	public function get_cls_nme(){
+		return $this->cls_nme;
+	}
+
+}
+?>
